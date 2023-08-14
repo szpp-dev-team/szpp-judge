6bb1348219e0fc8f0379cb8baf7fcf89d0224825
earlgray <earlgray283@gmail.com>
2023-08-14 19:22:21 +0900

    fix: use DBAddr instead of DBHost and DBPort

--- backend/core/config/config.go
+++ backend/core/config/config.go
@@ -5,8 +5,7 @@ import "github.com/kelseyhightower/envconfig"
 type Config struct {
 	DBUser   string `envconfig:"MYSQL_USER" required:"true"`
 	DBPass   string `envconfig:"MYSQL_PASSWORD" required:"true"`
-	DBHost   string `envconfig:"MYSQL_HOST" required:"true"`
-	DBPort   string `envconfig:"MYSQL_PORT" required:"true"`
+	DBAddr   string `envconfig:"BACKEND_DB_ADDR" required:"true"`
 	DBName   string `envconfig:"MYSQL_DATABASE" required:"true"`
 	GrpcPort string `envconfig:"BACKEND_GRPC_PORT" default:"50051"`
 	ModeDev  bool   `envconfig:"MODE_DEV" default:"true"`
