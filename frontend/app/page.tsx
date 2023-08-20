b2bb456d532dad901895b3a4026cfe369c4d9c7c
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-08-20 14:20:31 +0900

    chore: using Next.js pages router

--- frontend/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() { // TODO: cool name
-  return (
-    <p>
-      This page will be at /.
-    </p>
-  )
-}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Page: NextPage = () => { // TODO: cool name
+  return (
+    <p>
+      This page will be at /.
+    </p>
+  )
+}
+
+export default Page
