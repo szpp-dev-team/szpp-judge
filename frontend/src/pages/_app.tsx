7aff83f3edbbe589d7a57443ce14de36eae9af46
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-08-26 19:39:56 +0900

    style(frontend): apply dprint

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
