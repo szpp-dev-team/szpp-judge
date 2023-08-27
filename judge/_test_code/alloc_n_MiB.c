b48cdb964eab4be94f7b695615aa07e1629109f7
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-08-27 20:56:34 +0900

    chore(judge): _test_code/alloc_n_MiB.c にてメモリ使用量表示

--- judge/_test_code/alloc_n_MiB.c
+++ judge/_test_code/alloc_n_MiB.c
@@ -1,11 +1,37 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/resource.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 // stdin から整数 n を読み取って `n` MiB の領域を確保する。
 
+void show_stats(const char *title) {
+  char buf[64];
+
+  printf("============ %s ===========\n", title);
+  fflush(stdout);
+  fflush(stderr);
+
+  system("ps aux");
+  fflush(stdout);
+
+  pid_t const pid = getpid();
+  sprintf(buf, "grep '^Vm' /proc/%d/status", pid);
+  system(buf);
+
+  struct rusage u;
+  getrusage(RUSAGE_SELF, &u);
+  printf("maxRSS from getrusage(): %lu [MiB]\n", u.ru_maxrss / 1024);
+  fflush(stdout);
+}
+
 int main() {
+  printf("Self pid = %d\n", getpid());
+  show_stats("Initial");
+
   size_t n;
   if (scanf("%zu", &n) != 1) {
     fprintf(stderr, "Failed to read an integer from stdin");
@@ -33,6 +59,13 @@ int main() {
   }
   printf("a[%d] = %d\n", 0, a[0]);
   printf("a[%zu] = %d\n", len - 1, a[len - 1]);
+
+  show_stats("allocating");
+  usleep(1000 * 500);
+
+  free(a);
+
+  show_stats("after free");
 
   return 0;
 }
