26c08278680e521ba3045c9a4dbdf1bac87f3973
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-08-30 07:04:29 +0900

    fix(frontend): zodのデフォルトバリデーションエラーメッセージをカスタマイズ

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,11 @@
+import { defaultErrorMap } from "@/src/config/zod";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { z } from "zod";
 
 import("../mocks").catch((reason) => console.error(reason));
+
+z.setErrorMap(defaultErrorMap);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
