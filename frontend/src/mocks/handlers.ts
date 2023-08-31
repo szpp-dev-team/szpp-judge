23091683694eb8c7dd3ee985626c9b207fdf087e
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-01 03:41:08 +0900

    mock(frontend): AuthService/Login

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,7 +1,7 @@
-import { HealthcheckService } from "@/src/gen/proto/backend/v1/services_connect";
+import { AuthService, HealthcheckService } from "@/src/gen/proto/backend/v1/services_connect";
 import { createAsyncIterable, encodeEnvelope, readAllBytes, UniversalServerResponse } from "@bufbuild/connect/protocol";
 import { grpcStatusOk, trailerFlag, trailerSerialize } from "@bufbuild/connect/protocol-grpc-web";
-import { Message as ProtobufMessage } from "@bufbuild/protobuf";
+import { Message as ProtobufMessage, Timestamp } from "@bufbuild/protobuf";
 import { RequestHandler as MSWRequestHandler, ResponseTransformer, rest } from "msw";
 
 // Thanks to this blog and this issue.
@@ -42,8 +42,10 @@ const createGrpcResponse = <Response extends ProtobufMessage>(message: Response)
 const grpcResToMSWRes = (grpcResponse: ReturnType<typeof createGrpcResponse>): ResponseTransformer => {
   return async (res) => {
     const iterable = grpcResponse.body;
+    console.log("grpcResponse.body:", iterable);
     if (iterable) {
       res.body = await readAllBytes(iterable, Number.MAX_SAFE_INTEGER);
+      console.log("res.body:", res.body);
     }
     if (grpcResponse.header) {
       // eslint-disable-next-line no-unused-vars
@@ -54,6 +56,8 @@ const grpcResToMSWRes = (grpcResponse: ReturnType<typeof createGrpcResponse>): R
   };
 };
 
+const baseUrl = "http://localhost:8080/";
+
 export const handlers: MSWRequestHandler[] = [
   // eslint-disable-next-line no-unused-vars
   rest.post("http://localhost:8080/backend.v1.HealthcheckService/Ping", (_req, res, _ctx) => {
@@ -62,5 +66,34 @@ export const handlers: MSWRequestHandler[] = [
       respCls.fromJson({ message: "Hi Bob! Pong :)" }),
     );
     return res(grpcResToMSWRes(message));
+  }),
+
+  rest.post(`${baseUrl}${AuthService.typeName}/Login`, async (req, res, ctx) => {
+    const ReqClass = AuthService.methods.login.I;
+    const RespClass = AuthService.methods.login.O;
+
+    const a = await req.arrayBuffer();
+    const { username, password } = ReqClass.fromBinary(new Uint8Array(a.slice(5)));
+
+    const ok = (username === "user" || username === "admin") && password === "pass";
+    if (!ok) {
+      return res(
+        ctx.delay(500),
+        ctx.status(401), // 401 Forbidden
+      );
+    }
+    return res(
+      ctx.delay(500),
+      grpcResToMSWRes(createGrpcResponse(
+        new RespClass({
+          user: {
+            id: 1,
+            username,
+            isAdmin: username === "admin",
+            createdAt: Timestamp.now(),
+          },
+        }),
+      )),
+    );
   }),
 ];
