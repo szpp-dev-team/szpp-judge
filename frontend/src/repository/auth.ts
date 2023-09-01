745da8a042fb08647cbcd7edf13ae045b7f9302e
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-01 14:57:04 +0900

    refactor(frontend): src/gen/ 以下のファイルを絶対パスでimport

--- frontend/src/repository/auth.ts
+++ frontend/src/repository/auth.ts
@@ -1,7 +1,7 @@
+import { LoginRequest, LogoutRequest } from "@/src/gen/proto/backend/v1/messages_pb";
 import { AuthService } from "@/src/gen/proto/backend/v1/services_connect";
 import { createPromiseClient, PromiseClient, Transport } from "@bufbuild/connect";
 import { backendGrpcTransport } from "../config/grpc";
-import { LoginRequest, LogoutRequest } from "../gen/proto/backend/v1/messages_pb";
 import type { AuthUser } from "../model/user";
 
 export class AuthRepository {
