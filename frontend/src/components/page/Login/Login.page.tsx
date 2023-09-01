189a70386ea7d1207dec0c4dfb03459a68cb4f70
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-01 16:16:25 +0900

    fix(frontend): 各ページで<title>を設定可能に + <html>にlang=jaを設定

--- frontend/src/components/page/Login/Login.page.tsx
+++ frontend/src/components/page/Login/Login.page.tsx
@@ -1,9 +1,14 @@
+import { pageTitle } from "@/src/util/pagemeta";
+import Head from "next/head";
 import { WithHeaderFooter } from "../../layout/WithHeaderFooter";
 import { Login } from "./Login";
 
 export const LoginPage = () => {
   return (
     <WithHeaderFooter>
+      <Head>
+        <title>{pageTitle("ログイン")}</title>
+      </Head>
       <Login />
     </WithHeaderFooter>
   );
