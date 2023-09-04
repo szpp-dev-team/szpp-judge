278db0ace89f8e63069a9398c96faabf29da3e0f
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-04 13:08:37 +0900

    refactor(frontend): DifficultyBadge: remove shim component

--- frontend/src/components/model/task/DifficultyBadge.tsx
+++ frontend/src/components/model/task/DifficultyBadge.tsx
@@ -1,4 +1,4 @@
-import { Difficulty } from "@/src/model/task";
+import type { Difficulty } from "@/src/model/task";
 import { capitalize } from "@/src/util/string";
 import { Box, type BoxProps } from "@chakra-ui/react";
 import type { FC } from "react";
@@ -7,9 +7,26 @@ export type DifficultyBadgeProps = {
   dif: Difficulty;
 } & Omit<BoxProps, "children">;
 
-const Base = ({ d, ...props }: {
-  d: Difficulty;
-} & Omit<BoxProps, "children">) => {
+export const DifficultyBadge: FC<DifficultyBadgeProps> = ({ dif, ...props }) => {
+  const p = ((): BoxProps => {
+    switch (dif) {
+      case "beginner":
+        return { bg: "gray.200", borderColor: "gray.400", color: "gray.700" };
+      case "easy":
+        return { bg: "green.100", borderColor: "green.500", color: "green.800" };
+      case "medium":
+        return { bg: "cyan.100", borderColor: "cyan.500", color: "cyan.900" };
+      case "hard":
+        return { bg: "orange.100", borderColor: "orange.500", color: "orange.700" };
+      case "impossible":
+        return { bg: "black", borderColor: "#E56E6E", color: "#FF7C7C", fontWeight: "bold" };
+      default: {
+        const exhaustiveCheck: never = dif;
+        throw new Error(`Invlid difficulty: ${exhaustiveCheck}`);
+      }
+    }
+  })();
+
   return (
     <Box
       borderRadius="999px"
@@ -22,28 +39,10 @@ const Base = ({ d, ...props }: {
       border="1px"
       borderStyle="solid"
       fontSize="xs"
+      {...p}
       {...props}
     >
-      {capitalize(d)}
+      {capitalize(dif)}
     </Box>
   );
-};
-
-export const DifficultyBadge: FC<DifficultyBadgeProps> = ({ dif, ...props }) => {
-  switch (dif) {
-    case "beginner":
-      return <Base d={dif} bg="gray.200" borderColor="gray.400" color="gray.700" {...props} />;
-    case "easy":
-      return <Base d={dif} bg="green.100" borderColor="green.500" color="green.800" {...props} />;
-    case "medium":
-      return <Base d={dif} bg="cyan.100" borderColor="cyan.500" color="cyan.900" {...props} />;
-    case "hard":
-      return <Base d={dif} bg="orange.100" borderColor="orange.500" color="orange.700" {...props} />;
-    case "impossible":
-      return <Base d={dif} bg="black" borderColor="#E56E6E" color="#FF7C7C" fontWeight="bold" {...props} />;
-    default: {
-      const exhaustiveCheck: never = dif;
-      throw new Error(`Invlid difficulty: ${exhaustiveCheck}`);
-    }
-  }
 };
