61f7c4f37105f010a1570d48e649a6b8b3cc95e1
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-06 19:17:36 +0900

    feat(judge): impl GetGoModAbsDir()

--- judge/util/fsutil/fsutil.go
+++ judge/util/fsutil/fsutil.go
@@ -3,7 +3,25 @@ package fsutil
 import (
 	"io"
 	"os"
+	"path"
+	"runtime"
 )
+
+var judgeGoModDir string
+
+func init() {
+	_, testSourceFilePath, _, _ := runtime.Caller(0)
+	judgeGoModDir = path.Clean(path.Join(
+		path.Dir(testSourceFilePath),
+		"..",
+		"..",
+	))
+}
+
+// go.mod のあるディレクトリの絶対パスを返す
+func GetGoModAbsDir() string {
+	return judgeGoModDir
+}
 
 func CopyFile(srcPath string, dstPath string) error {
 	src, err := os.Open(srcPath)
