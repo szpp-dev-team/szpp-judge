bff17c5af4a72f6d3369fa334363dd74bfbb2f17
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-07 11:49:15 +0900

    chore(frontend): add `bordered-narrow` variant for Table

--- frontend/src/stories/chakra-extend/Table.stories.tsx
+++ frontend/src/stories/chakra-extend/Table.stories.tsx
@@ -10,7 +10,7 @@ const meta = {
   argTypes: {
     variant: {
       control: "select",
-      options: ["simple", "striped", "unstyled", "bordered"],
+      options: ["simple", "striped", "unstyled", "bordered", "bordered-narrow"],
     },
   },
 } satisfies Meta<typeof Table>;
@@ -18,35 +18,44 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const render: StoryObj["render"] = ({ ...props }) => (
+  <Table {...props}>
+    <Thead>
+      <Tr>
+        <Th textAlign="center">#</Th>
+        <Th textAlign="center">難易度</Th>
+        <Th textAlign="left">タイトル</Th>
+        <Th textAlign="center">自分の提出結果</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {[
+        [1, "Beginner", "すずっぴー君のおつかい", "AC"],
+        [2, "Easy", "すずっぴー君のおつかい", "WA"],
+        [3, "Easy", "すずっぴー君のおつかい", "TLE"],
+        [4, "Hard", "すずっぴー君のおつかい", "13/57 ..."],
+      ].map(([seq, d, title, s]) => (
+        <Tr key={seq}>
+          <Td textAlign="center">{seq}</Td>
+          <Td textAlign="center">{d}</Td>
+          <Td textAlign="left">{title}</Td>
+          <Td textAlign="center">{s}</Td>
+        </Tr>
+      ))}
+    </Tbody>
+  </Table>
+);
+
 export const Bordered: Story = {
   args: {
     variant: "bordered",
   },
-  render: ({ ...props }) => (
-    <Table {...props}>
-      <Thead>
-        <Tr>
-          <Th textAlign="center">#</Th>
-          <Th textAlign="center">難易度</Th>
-          <Th textAlign="left">タイトル</Th>
-          <Th textAlign="center">自分の提出結果</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {[
-          [1, "Beginner", "すずっぴー君のおつかい", "AC"],
-          [2, "Easy", "すずっぴー君のおつかい", "WA"],
-          [3, "Easy", "すずっぴー君のおつかい", "TLE"],
-          [4, "Hard", "すずっぴー君のおつかい", "13/57 ..."],
-        ].map(([seq, d, title, s]) => (
-          <Tr key={seq}>
-            <Td textAlign="center">{seq}</Td>
-            <Td textAlign="center">{d}</Td>
-            <Td textAlign="left">{title}</Td>
-            <Td textAlign="center">{s}</Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
-  ),
+  render,
+};
+
+export const BorderedNarrow: Story = {
+  args: {
+    variant: "bordered-narrow",
+  },
+  render,
 };
