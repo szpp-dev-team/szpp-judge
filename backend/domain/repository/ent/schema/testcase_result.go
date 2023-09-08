fb265f7394e86cbe9cf01b0a901ec1f4f19b92fc
mistralll <me.pooh441@gmail.com>
2023-09-08 15:44:55 +0900

    feat: testcase result

--- backend/domain/repository/ent/schema/testcase_result.go
+++ backend/domain/repository/ent/schema/testcase_result.go
@@ -1,7 +1,28 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 type TestcaseResult struct {
 	ent.Schema
+}
+
+func (TestcaseResult) Field() []ent.Field {
+	return []ent.Field{
+		field.Int("id"),
+		field.Int("submission_id"),
+		field.Int("testcase_id"),
+		field.String("status"),
+		field.Int("exec_time"), // ms
+		field.Int("exec_memory"), // kib
+	}
+}
+
+func (TestcaseResult) Edge() []ent.Edge {
+	return []ent.Edge {
+		edge.From("submit", Submit.Type),
+	}
 }
