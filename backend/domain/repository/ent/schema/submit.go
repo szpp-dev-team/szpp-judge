05b616026add533bf758c5449a649831b9c519d7
mistralll <me.pooh441@gmail.com>
2023-09-08 14:57:50 +0900

    feat: submit schema

---
+++ backend/domain/repository/ent/schema/submit.go
@@ -0,0 +1,36 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+type Submit struct {
+	ent.Schema
+}
+
+func (Submit) Field() []ent.Field {
+	return []ent.Field {
+		field.Int("id"),
+		field.Int("user_id"),
+		field.Int("contest_id").Optional(),
+		field.Int("task_id"),
+		field.Int("language_id"),
+		field.String("status"),
+		field.Int("exec_time"), // ms
+		field.Int("exec_memory"), // kib
+		field.Time("submitted_at"),
+		field.Time("created_at"),
+		field.Time("updated_at"),
+	}
+}
+
+func (Submit) Edges() []ent.Edge {
+	return []ent.Edge {
+		edge.To("user", User.Type),
+		// edge.To("task", Task.Type),
+		edge.To("language", Language.Type),
+		edge.To("testcase_result", TestcaseResult.Type),
+	}
+}
