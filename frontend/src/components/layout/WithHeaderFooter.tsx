997af7043823d5e47ddde7b8bd2a51f73b84fc93
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-09 19:52:54 +0900

    fix(frontend): add props to WithHeaderFooter

--- frontend/src/components/layout/WithHeaderFooter.tsx
+++ frontend/src/components/layout/WithHeaderFooter.tsx
@@ -1,20 +1,28 @@
-import { Box, Flex } from "@chakra-ui/react";
-import { type FC, ReactNode } from "react";
-import { GlobalFooter } from "../ui/GlobalFooter";
-import { GlobalHeader } from "../ui/GlobalHeader";
+import { Box, type BoxProps, Flex } from "@chakra-ui/react";
+import { type ReactNode } from "react";
+import { GlobalFooter, type GlobalFooterProps } from "../ui/GlobalFooter";
+import { GlobalHeader, type GlobalHeaderProps } from "../ui/GlobalHeader";
 
 export type WithHeaderFooterProps = {
   children: ReactNode;
+  headerProps?: GlobalHeaderProps;
+  bodyProps?: BoxProps;
+  footerProps?: GlobalFooterProps;
 };
 
-export const WithHeaderFooter: FC<WithHeaderFooterProps> = ({ children }) => {
+export const WithHeaderFooter = ({
+  children,
+  headerProps,
+  bodyProps,
+  footerProps,
+}: WithHeaderFooterProps) => {
   // 子要素で height=100% がうまく機能するためには、親要素で height が auto になってはならない
   // そこで、適当に height=1px を設定することで非 auto にするというトリックを施している
   return (
     <Flex flexDirection="column" minH="100vh" h="1px">
-      <GlobalHeader />
-      <Box as="main" flexGrow={1} bg="gray.200">{children}</Box>
-      <GlobalFooter mt="auto" />
+      <GlobalHeader {...headerProps} />
+      <Box as="main" flexGrow={1} bg="gray.200" {...bodyProps}>{children}</Box>
+      <GlobalFooter {...footerProps} />
     </Flex>
   );
 };
