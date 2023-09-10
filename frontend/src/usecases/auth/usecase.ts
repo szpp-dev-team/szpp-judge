bc55e07c49e46b96e5a6f129be64910269a890b3
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-09-10 17:52:24 +0900

    chore(frontend): ログイン時に認証情報等の atom を更新するようにした

--- frontend/src/usecases/auth/usecase.ts
+++ frontend/src/usecases/auth/usecase.ts
@@ -1,12 +1,19 @@
+import { useCredential } from "@/src/globalStates/credential";
+import { useUser } from "@/src/globalStates/user";
 import { AuthUser } from "@/src/model/user";
 import { authRepo, AuthRepository } from "@/src/repository/auth";
 
-const createAuthUsecase = ({ repository }: { repository: AuthRepository }) => {
+const createAuthUsecase = ({ repository, setCredential, setUser }: { repository: AuthRepository, setCredential: ReturnType<typeof useCredential>[1], setUser: ReturnType<typeof useUser>[1] }) => {
   return {
     // rest parameter を使うとこの関数で使いたい repository.<method> が1つなら
     // いいけどそれ以上のことをやらせたいときに引数が自由に使えなくて不便かも
     async login(...args: Parameters<typeof repository.login>): Promise<AuthUser> {
       const resp = await repository.login(...args);
+
+      // globalState 更新
+      setCredential({ refreshToken: "tooooooooooooooooken" }) // TODO: ちゃんとトークンをセットする
+      setUser(resp)
+
       return resp;
     },
     async logout(): Promise<void> {
@@ -15,5 +22,9 @@ const createAuthUsecase = ({ repository }: { repository: AuthRepository }) => {
 };
 
 export const useAuthUsecase = () => {
-  return createAuthUsecase({ repository: authRepo }); // TODO: React.memo
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_c, setCredential] = useCredential();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_u, setUser] = useUser();
+  return createAuthUsecase({ repository: authRepo, setCredential, setUser }); // TODO: React.memo
 };
