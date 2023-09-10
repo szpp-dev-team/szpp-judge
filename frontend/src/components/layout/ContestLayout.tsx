53ca800719afa63f6469b7fd5eed48f49665519f
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-10 20:45:08 +0900

    chore(frontend): add ContestLayout

---
+++ frontend/src/components/layout/ContestLayout.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@chakra-ui/react";
+import type { ReactNode } from "react";
+import { ContestSidebar } from "../model/contest/ContestSidebar";
+import { WithHeaderFooter } from "./WithHeaderFooter";
+
+export type ContestLayoutProps = {
+  children: ReactNode;
+};
+
+export const ContestLayout = ({ children }: ContestLayoutProps) => {
+  const headerH = "3.25rem";
+  return (
+    <WithHeaderFooter
+      headerProps={{ contestSlug: "sbc001", contestTitle: "SZPP Beginners Contest 001" }}
+      bodyProps={{ as: "div", display: "flex" }}
+    >
+      <ContestSidebar
+        top={headerH}
+        startAt={new Date("2023-09-02 21:00")}
+        endAt={new Date("2023-09-04 01:00")}
+        now={new Date("2023-09-02 21:00:01")}
+        slug="sbc001"
+        tasks={[
+          { id: 1000, title: "すずっぴー君のおつかい" },
+          { id: 1001, title: "すずっぴー君の変死", scoreStatus: "perfect" },
+          { id: 1002, title: "すずっぴー君の怪死", scoreStatus: "perfect" },
+          { id: 1003, title: "すずっぴー君の爆死", scoreStatus: "zero" },
+          { id: 1004, title: "すずっぴー君の溺死" },
+          { id: 1005, title: "すずっぴー君の圧死", scoreStatus: "partial" },
+          { id: 1006, title: "すずっぴー君の尊死", scoreStatus: "zero" },
+          { id: 1007, title: "長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル" },
+          { id: 1008, title: "longlonglonglonglonglonglonglonglonglonglonglong", scoreStatus: "partial" },
+          { id: 1009, title: "すずっぴー君のおつかい" },
+        ]}
+      />
+      <Box as="main">{children}</Box>
+    </WithHeaderFooter>
+  );
+};
