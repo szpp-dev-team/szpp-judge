fdf2fd423d800b0ffbb531a36b55f94d5e647d0b
evipepota <shrimpround12@gmail.com>
2023-09-12 16:42:53 +0900

    fix(proto): set username to unique

--- backend/domain/repository/ent/schema/user.go
+++ backend/domain/repository/ent/schema/user.go
@@ -14,7 +14,7 @@ type User struct {
 func (User) Fields() []ent.Field {
 	return []ent.Field{
 		field.Int("id"),
-		field.String("name"),
+		field.String("name").Unique(),
 		field.String("role"), // this field's type must be managed with proto
 		field.String("encrypted_password"),
 		field.Time("created_at"),
