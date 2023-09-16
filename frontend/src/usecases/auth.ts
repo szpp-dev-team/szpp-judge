70195e1daf2b4e2592659b5861431f625b98ead2
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-09-16 17:21:23 +0900

    refactor(frontend): extract error handling logic from Login.tsx

--- frontend/src/usecases/auth.ts
+++ frontend/src/usecases/auth.ts
@@ -1,7 +1,29 @@
+import { Code, ConnectError } from "@bufbuild/connect";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../gen/proto/backend/v1/services-AuthService_connectquery";
 
-export const useLogin = () => {
-  const loginMutation = useMutation(login.useMutation());
-  return { loginMutation };
+/**
+ * - 使うときは mutate を呼び出す
+ * - mutate を使う限りエラーは react-query で握りつぶしてくれるが username, password の
+ *   間違いが原因のエラーに関してはコンポーネントに通知したいのでコールバックがある
+ * @param onUnauthenticatedError
+ * @returns
+ */
+export const useLogin = (onUnauthenticatedError?: () => void) => {
+  const onConnectError = (e: ConnectError) => {
+    if (e.code === Code.Unauthenticated && onUnauthenticatedError) {
+      onUnauthenticatedError();
+    } else {
+      throw e;
+    }
+  };
+
+  const { data, error, isLoading, mutate, mutateAsync } = useMutation({
+    ...login.useMutation({ onError: onConnectError }),
+    onSuccess: (data) => {
+      console.log("[usecase]onSuccess");
+    },
+  });
+
+  return { data, error, isLoading, mutate, mutateAsync };
 };
