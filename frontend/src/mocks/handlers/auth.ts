de58a0dfb7b25a630681065d1a1a88ca44fa7b41
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-18 02:31:49 +0900

    mock(frontend): AuthService/Logout

--- frontend/src/mocks/handlers/auth.ts
+++ frontend/src/mocks/handlers/auth.ts
@@ -1,8 +1,36 @@
-import { LoginRequest, LoginResponse } from "@/src/gen/proto/backend/v1/messages_pb";
+import { LoginRequest, LoginResponse, LogoutRequest, LogoutResponse } from "@/src/gen/proto/backend/v1/messages_pb";
 import { AuthService } from "@/src/gen/proto/backend/v1/services_connect";
 import { Timestamp } from "@bufbuild/protobuf";
 import type { RequestHandler } from "msw";
 import { grpcMock } from "../grpc";
+
+const ACCESS_TOKEN_PREFIX = "eyJxxx";
+const REFRESH_TOKEN_PREFIX = "refreeeeesh";
+
+type Credential = Pick<LoginResponse, "accessToken" | "refreshToken">;
+type AccessTokenClaim = {
+  username: string;
+  isAdmin: boolean;
+};
+
+export const generateMockCredential = (username: string): Credential => {
+  const now = new Date().toLocaleString("sv-SE"); // JST で "YYYY-mm-dd HH:MM:SS" を返してくれる
+  return {
+    accessToken: `${ACCESS_TOKEN_PREFIX}/${username}/${now}`,
+    refreshToken: `${REFRESH_TOKEN_PREFIX}/${now}`,
+  };
+};
+
+// パースに失敗した場合 or 不正なトークンの場合は null を返す (モックなので厳密なチェックはしない)
+export const parseAccessToken = (accessToken: string): AccessTokenClaim | null => {
+  const a = accessToken.split("/");
+  const [prefix, username] = a;
+  if (a.length !== 3 || prefix !== ACCESS_TOKEN_PREFIX) {
+    return null;
+  }
+  const isAdmin = username === "admin";
+  return { username, isAdmin };
+};
 
 export const authHandlers: RequestHandler[] = [
   grpcMock(AuthService, "login", async (ctx, res, decodeReq, encodeResp) => {
@@ -25,10 +53,25 @@ export const authHandlers: RequestHandler[] = [
             isAdmin: username === "admin",
             createdAt: Timestamp.now(),
           },
-          accessToken: "eyJ..." + Date.now(), // 適当
-          refreshToken: "refreeeeesh" + Date.now(), // 適当
+          ...generateMockCredential(username),
         }),
       ),
+    );
+  }),
+
+  grpcMock(AuthService, "logout", async (ctx, res, decodeReq, encodeResp) => {
+    const { refreshToken } = await decodeReq() as LogoutRequest;
+
+    const ok = refreshToken.startsWith(REFRESH_TOKEN_PREFIX);
+    if (!ok) {
+      return res(
+        ctx.delay(500),
+        ctx.status(401),
+      );
+    }
+    return res(
+      ctx.delay(500),
+      encodeResp(new LogoutResponse()),
     );
   }),
 ];
