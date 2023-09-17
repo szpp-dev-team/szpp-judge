f02874ccd446d11be30d608b38c469add3532aa1
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-17 22:19:48 +0900

    refactor(frontend): usecase/contest.tsの内容をusecases/contest.tsへ移動 + genNthTaskSeq -> calcNthTaskSeq

--- frontend/src/usecases/contest.ts
+++ frontend/src/usecases/contest.ts
@@ -3,6 +3,20 @@ import { PartialMessage } from "@bufbuild/protobuf";
 import { useQuery } from "@tanstack/react-query";
 import { ListContestTasksRequest } from "../gen/proto/backend/v1/messages_pb";
 
+const CHAR_CODE_A = "A".charCodeAt(0);
+
+/**
+ * コンテストの問題数に応じて問題番号をアルファベットまたは整数で生成する。
+ * @param i 問題の添字 (0-indexed)
+ * @param numTotalTasks コンテストの問題数
+ */
+export const calcNthTaskSeq = (i: number, numTotalTasks: number): string => {
+  if (numTotalTasks < 26) {
+    return String.fromCharCode(CHAR_CODE_A + i);
+  }
+  return String(i + 1);
+};
+
 export const useListContestTasks = (input: PartialMessage<ListContestTasksRequest>) => {
   const contestTasksQuery = useQuery(listContestTasks.useQuery(input));
   return { contestTasksQuery };
