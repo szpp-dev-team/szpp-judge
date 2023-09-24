d388389271f20f217efe9fd8252eafb7bb6ce784
piddy-shz <shoji.mt19937@outlook.jp>
2023-09-25 08:36:27 +0900

    add page title to /contests

--- frontend/src/components/page/ContestList/ContestList.tsx
+++ frontend/src/components/page/ContestList/ContestList.tsx
@@ -1,23 +1,39 @@
 import { Link } from "@/src/components/ui/Link";
 import { useListContests } from "@/src/usecases/contest";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading
+} from "@chakra-ui/react";
 
 export const ContestList = () => {
   const { contests } = useListContests({});
   return (
-    <div>
-      <strong>動作確認用</strong>
-      <ul>
-        <li>
-          <Link href="/contests/not_exist/tasks">
-            Go to <code>note_exist/tasks</code>
-          </Link>
-        </li>
-        {contests?.map((c) => (
-          <li key={c.id}>
-            <Link href={`/contests/${c.slug}`}>Go to {c.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Box px={16} h="100%">
+      <Card px={3} py={4} h="100%" borderRadius={0}>
+        <CardHeader>
+          <Heading as="h1">コンテスト一覧</Heading>
+        </CardHeader>
+        <CardBody>
+          <div>
+            <strong>動作確認用</strong>
+            <ul>
+              <li>
+                <Link href="/contests/not_exist/tasks">
+                  Go to <code>note_exist/tasks</code>
+                </Link>
+              </li>
+              {contests?.map((c) => (
+                <li key={c.id}>
+                  <Link href={`/contests/${c.slug}`}>Go to {c.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </CardBody>
+      </Card>
+    </Box>
   );
 };
