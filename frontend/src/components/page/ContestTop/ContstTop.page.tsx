5531a132ea399268a71566fc37f9b96339586c00
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-09-24 14:34:59 +0900

    refactor(frontend): contestTitle()を削除して代わりにuse*PageTitle()を使用

--- frontend/src/components/page/ContestTop/ContstTop.page.tsx
+++ frontend/src/components/page/ContestTop/ContstTop.page.tsx
@@ -1,13 +1,14 @@
-import { pageTitle } from "@/src/util/pagemeta";
+import { useContestPageTitle } from "@/src/usecases/pagemeta";
 import Head from "next/head";
 import { ContestLayout } from "../../layout/ContestLayout";
 import { ContestTop } from "./ContestTop";
 
 export const ContestTopPage = () => {
+  const title = useContestPageTitle("トップ");
   return (
     <ContestLayout>
       <Head>
-        <title>{pageTitle("コンテストトップ")}</title>
+        <title>{title}</title>
       </Head>
       <ContestTop />
     </ContestLayout>
