febc2adcf5ed068a40bcbe5ef15e8658768e0507
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-09-26 01:55:48 +0900

    chore(frontend): ダミーのジャッジ結果データを追加

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -2,9 +2,9 @@ import type { RequestHandler } from "msw";
 import { authHandlers } from "./auth";
 import { contestHandlers } from "./contest";
 import { healthcheckHandlers } from "./healthcheck";
+import { judgeHandlers } from "./judge";
 import { taskHandlers } from "./task";
 import { userHandlers } from "./user";
-import { judgeHandlers } from "./judge";
 
 export const handlers: readonly RequestHandler[] = [
   ...healthcheckHandlers,
--- frontend/src/mocks/handlers/judge.ts
+++ frontend/src/mocks/handlers/judge.ts
@@ -1,3 +1,61 @@
+import type { SubmissionDetail } from "@/src/gen/proto/backend/v1/judge_resources_pb";
+import { JudgeService } from "@/src/gen/proto/backend/v1/judge_service-JudgeService_connectquery";
+import { JudgeStatus } from "@/src/gen/proto/judge/v1/resources_pb";
+import { PlainMessage, Timestamp } from "@bufbuild/protobuf";
 import type { RequestHandler } from "msw";
+import { grpcMock } from "../grpc";
 
-export const judgeHandlers: RequestHandler[] = [];
+const sourcecodeAc = String.raw`#include <iostream>
+using namespace std;
+
+int main() {
+  int i;
+  cin >> i;
+
+  if (i % 15 == 0)
+    cout << "Fizz Buzz" << endl;
+  else if (i % 3 == 0)
+    cout << "Fizz" << endl;
+  else if (i % 5 == 0)
+    cout << "Buzz" << endl;
+  else
+    cout << i << endl;
+
+  return 0;
+}
+`;
+
+const dummySubmissionDetails: PlainMessage<SubmissionDetail>[] = [
+  {
+    id: 1,
+    userId: 1,
+    username: "user",
+    contestId: 103,
+    taskId: 1,
+    taskTitle: "すずっぴー君のおつかい",
+    score: 100,
+    langId: "C++",
+    sourceCode: sourcecodeAc,
+    status: JudgeStatus.AC,
+    execTimeMs: 1,
+    execMemoryKib: 3000,
+    testcaseResults: [
+      { testcaseName: "Sample", judgeStatus: JudgeStatus.AC, execTimeMs: 1, execMemoryKib: 3000 },
+      { testcaseName: "All", judgeStatus: JudgeStatus.AC, execTimeMs: 1, execMemoryKib: 3000 },
+    ],
+    submittedAt: Timestamp.fromDate(new Date("2023-09-25")),
+  },
+];
+
+export const judgeHandlers: RequestHandler[] = [
+  grpcMock(JudgeService, "getSubmissionDetail", async (ctx, res, decodeReq, encodeResp) => {
+    const { id } = await decodeReq();
+
+    const submissionDetail = dummySubmissionDetails.filter(submission => submission.id === id);
+    if (submissionDetail.length > 0) {
+      return res(encodeResp({ submissionDetail: submissionDetail[0] }));
+    } else {
+      return res(ctx.status(404));
+    }
+  }),
+];
