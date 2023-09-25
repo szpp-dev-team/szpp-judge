5e8ec4321f9a119b82d1e21b4d28c4eb68359fcf
piddy-shz <shoji.mt19937@outlook.jp>
2023-09-25 09:44:12 +0900

    add 'endAt' to table

--- frontend/src/components/page/ContestList/ContestList.tsx
+++ frontend/src/components/page/ContestList/ContestList.tsx
@@ -28,7 +28,8 @@ export const ContestList = () => {
             <Table variant="bordered-narrow">
               <Thead>
                 <Tr>
-                  <Th textAlign="center">開始日時</Th>
+                  <Th textAlign="center">開始</Th>
+                  <Th textAlign="center">終了</Th>
                   <Th textAlign="center">コンテスト名</Th>
                   <Th textAlign="center">問題数</Th>
                 </Tr>
@@ -37,6 +38,7 @@ export const ContestList = () => {
                 {contests?.map((c) => (
                   <Tr key={c.slug}>
                     <Td textAlign="center">{c.startAt?.toDate().toLocaleString()}</Td>
+                    <Td textAlign="center">{c.endAt?.toDate().toLocaleString()}</Td>
                     <Td textAlign="left">
                       <Link href={`/contests/${c.slug}`}>{c.name}</Link>
                     </Td>
