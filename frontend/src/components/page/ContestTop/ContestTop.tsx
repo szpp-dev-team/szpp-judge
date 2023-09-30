47642cf556dff48fe01b0ab5c95d2e241ee13a39
piddy-shz <shoji.mt19937@outlook.jp>
2023-09-30 19:29:38 +0900

    WIP: impl contest top

--- frontend/src/components/page/ContestTop/ContestTop.tsx
+++ frontend/src/components/page/ContestTop/ContestTop.tsx
@@ -4,12 +4,39 @@ import {
   Card,
   CardHeader,
   Heading,
-  CardBody
+  CardBody,
+  Button,
 } from "@chakra-ui/react";
+//import { useTimer } from "react-timer-hook";
 
 export const ContestTop = () => {
   const slug = useRouterContestSlug();
   const { contest } = useGetContest({slug});
+
+  /*let expiryTimestamp: Date;
+  const date = new Date();
+  if (contest?.startAt != undefined && date < contest.startAt.toDate()) {
+    expiryTimestamp = contest.startAt.toDate();
+  } else if (contest?.endAt != undefined && date <= contest.endAt.toDate()) {
+    expiryTimestamp = contest.endAt.toDate();
+  } else {
+    expiryTimestamp = date;
+  }
+  expiryTimestamp = date;
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 10);
+
+  const timer = useTimer({expiryTimestamp});
+
+  let seq = [
+    timer.days.toLocaleString(),
+    timer.hours.toLocaleString(),
+    timer.minutes.toLocaleString(),
+    timer.seconds.toLocaleString()
+  ];
+  for (let i = 0; i < 4; i++) {
+    if (seq[i].length == 1) seq[i] = "0" + seq[i];
+  }*/
+
   return (
     <Box px={16} h="100%">
       <Card px={3} py={4} h="100%" borderRadius={0}>
@@ -21,6 +48,9 @@ export const ContestTop = () => {
             開催期間: {contest?.startAt?.toDate().toLocaleString()} - {contest?.endAt?.toDate().toLocaleString()}
           </p>
           <p>{contest?.description}</p>
+          <Button colorScheme="teal">
+            参加登録
+          </Button>
         </CardBody>
       </Card>
     </Box>
