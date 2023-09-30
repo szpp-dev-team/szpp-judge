cddb69b34b44259517685f471ca18dd640f83a4e
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-10-01 02:12:08 +0900

    refactor(frontend): s/backendGrpcTransport/backendGrpcTransportWithOnlyBaseUrl/

--- frontend/src/config/grpc.ts
+++ frontend/src/config/grpc.ts
@@ -1,4 +1,7 @@
 import { createGrpcWebTransport } from "@bufbuild/connect-web";
 
 export const backendGrpcBaseUrl = process.env.NEXT_PUBLIC_ENVOY_BASE_URL!;
-export const backendGrpcTransport = createGrpcWebTransport({ baseUrl: backendGrpcBaseUrl });
+
+export const backendGrpcTransportWithOnlyBaseUrl = createGrpcWebTransport({
+  baseUrl: backendGrpcBaseUrl,
+});
