e1500422ed79735015a8a2f1b4a83fe7205ff44a
piddy-shz <shoji.mt19937@outlook.jp>
2023-09-30 13:59:53 +0900

    feat: roughly impl contest top

--- frontend/src/components/page/ContestTop/ContestTop.tsx
+++ frontend/src/components/page/ContestTop/ContestTop.tsx
@@ -1,3 +1,28 @@
+import { useGetContest, useRouterContestSlug } from "@/src/usecases/contest";
+import {
+  Box,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody
+} from "@chakra-ui/react";
+
 export const ContestTop = () => {
-  return <></>;
+  const slug = useRouterContestSlug();
+  const { contest } = useGetContest({slug});
+  return (
+    <Box px={16} h="100%">
+      <Card px={3} py={4} h="100%" borderRadius={0}>
+        <CardHeader>
+          <Heading as="h1">{contest?.name}</Heading>
+        </CardHeader>
+        <CardBody>
+          <p>
+            開催期間: {contest?.startAt?.toDate().toLocaleString()} - {contest?.endAt?.toDate().toLocaleString()}
+          </p>
+          <p>{contest?.description}</p>
+        </CardBody>
+      </Card>
+    </Box>
+  );
 };
