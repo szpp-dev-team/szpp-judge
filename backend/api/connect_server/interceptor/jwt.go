b8206cbf589e86df53d29663ee3a6a83ca654f18
navleorange <navleorange.0718@gmail.com>
2023-10-06 15:49:44 +0900

    debug: print access token TrimPrefix

--- backend/api/connect_server/interceptor/jwt.go
+++ backend/api/connect_server/interceptor/jwt.go
@@ -48,7 +48,7 @@ func Auth(secret []byte) connect.UnaryInterceptorFunc {
 
 			accessToken := req.Header().Get("Authorization")
 			log.Println("AccessToken: " + accessToken)
-			claims, err := GetClaimsFromToken(strings.TrimPrefix(accessToken, "Bearer"), secret)
+			claims, err := GetClaimsFromToken(strings.TrimPrefix(accessToken, "Bearer "), secret)
 			if err != nil {
 				return nil, connect.NewError(connect.CodeUnauthenticated, errors.New("failed to parse jwt"))
 			}
