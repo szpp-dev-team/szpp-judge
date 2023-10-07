468720fa8a61d3c6de1ff6354d61f723044c34ce
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-10-07 21:36:48 +0900

    fix(frontend): MarkdownViewにBoxPropsを渡せるように

--- frontend/src/components/ui/MarkdownView.tsx
+++ frontend/src/components/ui/MarkdownView.tsx
@@ -1,15 +1,17 @@
 import { parseMarkdownToHtml } from "@/src/util/markdown";
 import { useMemo } from "react";
 import "katex/dist/katex.min.css";
+import { Box, type BoxProps } from "@chakra-ui/react";
 
 export type MarkdownViewProps = {
   markdown?: string;
-};
+} & Omit<BoxProps, "children">;
 
 export const MarkdownView = ({
   markdown,
+  ...props
 }: MarkdownViewProps) => {
   const html = useMemo(() => parseMarkdownToHtml(markdown ?? ""), [markdown]);
 
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  return <Box {...props} dangerouslySetInnerHTML={{ __html: html }} />;
 };
