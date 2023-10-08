f71df75e16a0daf4ff2ffb8f466c091e2267eb03
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-10-09 03:59:59 +0900

    feat(frontend): add top page

--- frontend/src/components/page/Top/Top.tsx
+++ frontend/src/components/page/Top/Top.tsx
@@ -1,3 +1,62 @@
+import { Card, CardBody, CardHeader, Grid, GridItem, Heading, HStack, Text } from "@chakra-ui/react";
+
 export const Top = () => {
-  return <h1>Here is the Top page!</h1>;
+  return (
+    <Grid
+      minH="100%"
+      gap={4}
+      pb={4}
+      templateAreas={`"hero hero hero"
+                      "contests tasks info"`}
+      templateRows="350px 1fr"
+      templateColumns="repeat(3, 1fr)"
+    >
+      <GridItem area="hero" bg="#212121">
+        <HStack gap={16} h="100%" justifyContent="center">
+          {/** output: "export" にしていると next/image による image optimization が使えない */}
+          <img src="/szpp-judge-logo.png" alt="logo image" width={293} height={293} />
+          <Text fontSize="lg" lineHeight={7} color="white" whiteSpace="pre-wrap">
+            {`SZPP Judge は静岡大学プログラミングサークル SZPP (すずっぷ) が運営する、
+競技プログラミング用オンラインジャッジサイトです。
+
+ビジュアルプログラミング言語 Scratch での提出にも対応しており、
+市民向けプログラミング講座などで活用しています。`}
+          </Text>
+        </HStack>
+      </GridItem>
+
+      <GridItem area="contests" display="flex" pl={4}>
+        <Card borderRadius={16} flex={1}>
+          <CardHeader textAlign="center">
+            <Heading size="md">最新のコンテスト</Heading>
+          </CardHeader>
+          <CardBody>
+            contest
+          </CardBody>
+        </Card>
+      </GridItem>
+
+      <GridItem area="tasks" display="flex">
+        <Card borderRadius={16} flex={1}>
+          <CardHeader textAlign="center">
+            <Heading size="md">最新の問題</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>準備中</Text>
+          </CardBody>
+        </Card>
+      </GridItem>
+
+      <GridItem area="info" display="flex" pr={4}>
+        <Card borderRadius={16} flex={1}>
+          <CardHeader textAlign="center">
+            <Heading size="md">お知らせ</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>準備中</Text>
+          </CardBody>
+        </Card>
+      </GridItem>
+    </Grid>
+  );
 };
