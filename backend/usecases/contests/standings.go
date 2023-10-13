95a7e82397c55bfbf3fc6cf73441eef223a91ccf
navleorange <navleorange.0718@gmail.com>
2023-10-13 20:08:46 +0900

    fix: stable sort to sort

--- backend/usecases/contests/standings.go
+++ backend/usecases/contests/standings.go
@@ -110,7 +110,7 @@ func GetStandingsRecordSlice(userInfo map[int]StandingsRecord) []StandingsRecord
 	log.Println(result)
 
 	// sort by totalScore and latestUntilAc
-	sort.SliceStable(result, func(i, j int) bool {
+	sort.Slice(result, func(i, j int) bool {
 		if result[i].totalScore > result[j].totalScore {
 			log.Println("==================sort By Score============================")
 			log.Println("i:" + strconv.Itoa(i) + " " + strconv.Itoa(result[i].totalScore) + " j:" + strconv.Itoa(j) + " " + strconv.Itoa(result[j].totalScore))
